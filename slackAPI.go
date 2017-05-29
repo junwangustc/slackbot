4f4589135ce5b4d6bfaeb0b39abc4117a4010737
junwangustc <junwangustc@gmail.com>
2017-05-29 07:34:05 +0000

    Add slack token test function

--- slackAPI.go
+++ slackAPI.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	slackPkg "github.com/nlopes/slack"
+)
 
 func main() {
 	fmt.Println("hello world")
+	api := slackPkg.New("Your-Token")
+	Users, err := api.GetUsers()
+	if err != nil {
+		fmt.Printf("Get Slack users info failed: %v", err)
+	}
+	for _, user := range Users {
+		fmt.Println(user.Profile.Email, user.ID)
+	}
+	Channels, err := api.GetChannels(true)
+	if err != nil {
+		fmt.Printf("Get Slack channels info failed: %v", err)
+	}
+	for _, channel := range Channels {
+		fmt.Println(channel.Name, channel.ID)
+	}
 
 }
